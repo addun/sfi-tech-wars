88604b179b5e7fb33b822b252dd5653d5f7fd02a
Adrian Bury <adrian.jakub.bury@gmail.com>
2021-04-12 23:09:20 +0200

    Base template without logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
-import { ErrorInterceptor } from './api/error.interceptor';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true,
-    },
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top',
+    }),
   ],
   bootstrap: [AppComponent],
 })
--- src/app/modules/http-errors/components/http-error-code.i18n.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'sfi-http-error-code',
-  template: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class HttpErrorCodeI18n {}
